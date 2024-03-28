e9b54891abae71429f031d650118c21c0cf6500e
Fatality <fatihcayir2001@gmail.com>
2024-03-28 03:33:21 +0300

    Work Queue

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -1,5 +1,6 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQ.Core;
+using System.Collections.Generic;
 using System.Text;
 
 var factory = new ConnectionFactory();
@@ -12,11 +13,18 @@ using (var connection = factory.CreateConnection())
     var queueName = Constant.QUEUE_NAME;
     channel.QueueDeclare(queueName, true, false, false);
 
-    string message = "Hello World";
-    var messageBody = Encoding.UTF8.GetBytes(message);
+    Enumerable.Range(1, 2).ToList().ForEach(x =>
+    {
+        string message = $"Mesaj {x}";
+        var messageBody = Encoding.UTF8.GetBytes(message);
 
-    channel.BasicPublish(string.Empty, queueName, null, messageBody);
+        channel.BasicPublish(string.Empty, queueName, null, messageBody);
 
-    Console.WriteLine("Mesaj gönderilmiştir");
+        Console.WriteLine($"Mesaj gönderilmiştir: {message}");
+    });
+
+    
+
+    
     Console.Read();
 }
--- RabbitMQ.Subsicriber/Program.cs
+++ RabbitMQ.Subsicriber/Program.cs
@@ -11,20 +11,20 @@ using (var connection = factory.CreateConnection())
     var channel = connection.CreateModel();
 
     var queueName = Constant.QUEUE_NAME;
-    channel.QueueDeclare(queueName, true, false, false);
+    var queue = channel.QueueDeclare(queueName, true, false, false);
+    channel.BasicQos(0, 1, false);//Herhangi bir void mesaji, mesajlar birer gelsin, global true ise tek bir seferde tüm subsicribirlera toplam x adet
 
     var consumer = new EventingBasicConsumer(channel);
 
-    channel.BasicConsume(queueName, true,  consumer);
+    channel.BasicConsume(queueName, false,  consumer); //true hemen sil false silme haberdar et, bu true kalinca birden fazla ayağa kalkdıysa gitmiyor ayrı ayrı
 
-    consumer.Received += Consumer_Received;
+    consumer.Received += (object? sender, BasicDeliverEventArgs e) => {
+        var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+        Console.WriteLine("Mesaj : " + message);
+        channel.BasicAck(e.DeliveryTag, false); //Kuyruktan sil
+
+    };
 
     Console.Read();
-}
-
-void Consumer_Received(object? sender, BasicDeliverEventArgs e)
-{
-    var message = Encoding.UTF8.GetString(e.Body.ToArray());
-
-    Console.WriteLine("Mesaj : " + message);
 }
